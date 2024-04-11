8945b2d4cdc3d4a24b1695a96f0568312f292880
xennaz <xennaz@gmail.com>
2024-04-11 15:49:10 +0300

    commit

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -1,6 +1,23 @@
 public class MainClass {
 
+    private int class_number = 20;
+    private String class_string = "Hello, world";
+
     public int getLocalNumber(){
+
         return 14;
     }
+
+    public int getClassNumber(){
+
+        return this.class_number;
+    }
+
+    public String getClassString(){
+        return this.class_string;
+    }
+
+
 }
+
+
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
@@ -6,7 +7,7 @@ public class MainClassTest extends MainClass {
 
     public void testGetLocalNumber(){
 
-          if (getLocalNumber() == 14){
+        if (getLocalNumber() == 14){
             System.out.println("Method Local number returns 14");
         } else {
             System.out.println("Method local number returns wrong number");
@@ -14,6 +15,22 @@ public class MainClassTest extends MainClass {
 
     }
 
+    @Test
+    public void testGetClassNumber(){
 
+        if (getClassNumber() > 45){
+            System.out.println("The returned number " + getClassNumber() +" > 45" );
+        } else {
+            System.out.println("Wrong returned number " + getClassNumber());
+        }
+    }
+
+    @Test
+
+    public void testGetClassString(){
+
+        String stringToLowerCase = getClassString().toLowerCase();
+        Assert.assertTrue("Word Hello/hello is absent", stringToLowerCase.contains("hello"));
+    }
 
 }
